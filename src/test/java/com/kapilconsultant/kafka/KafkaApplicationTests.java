7238b455453ccfa9cdd57d85ad8cf82572d20aab
kgizmo <the.vyases@gmail.com>
2025-10-14 20:54:40 -0400

    Recreated project with spring initializer.

---
+++ src/test/java/com/kapilconsultant/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapilconsultant.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
